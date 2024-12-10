90bccb17ce79c6f3d4b39fb4df39c67e92157ed1
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-09 21:13:12 -0300

    upload files

---
+++ source/c#-exercises/exercise-6/exercise-5/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_5 {
+    internal class Calculadora {
+
+        public double Pi = 3.14;
+
+        public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+
+        public double Circunferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+
+    }
+}
---
+++ source/c#-exercises/exercise-6/exercise-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using exercise_5;
+using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace exercise6 {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de PI: {calc.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+       
+    }
+}
