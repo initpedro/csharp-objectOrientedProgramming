c6ca1db1a3ea23f861f61055acfe307d0e4dad5e
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-09 21:13:45 -0300

    upload files

---
+++ source/c#-exercises/exercise-7/exercise-5/Program.cs
@@ -0,0 +1,24 @@
+﻿using exercise_7;
+using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace exercise7 {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+       
+    }
+}
