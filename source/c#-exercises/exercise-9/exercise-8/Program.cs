ecfd6c1145b7572a7e4dcb626b00812460f2e648
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-09 21:14:53 -0300

    upload files

---
+++ source/c#-exercises/exercise-9/exercise-8/Program.cs
@@ -0,0 +1,30 @@
+﻿using exercise_9;
+using System;
+using System.Globalization;
+
+namespace exercise9 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados do produto: {p}");
+            Console.WriteLine();
+            Console.Write("\nDigite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {p}");
+
+            Console.Write("\nDigite o número de produtos a ser removidos ao estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {p}");
+        }
+    }
+}
