bbc3308565ee9af50145471a11a52ac28d86530c
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-10 22:07:49 -0300

    up

---
+++ source/c#-exercises/exercise-9-revisado/exercise-8/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Net;
+
+namespace exercise_9 {
+    internal class Produto {
+        // Atributo privativo
+        private string _nome;
+
+        // Propriedades autoimplementadas
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        
+        // Construrtores
+        public Produto() {
+        }
+
+        // Construtores
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Propriedades customizadas
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+
+        // Outros métodos
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {
+            return $"{_nome}, $ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, {Quantidade} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ source/c#-exercises/exercise-9-revisado/exercise-8/Program.cs
@@ -0,0 +1,15 @@
+﻿using exercise_9;
+using System;
+using System.Globalization;
+
+namespace exercise9 {
+    internal class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
