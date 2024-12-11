aef249a21df913a388d5894e49ed7e0428c83163
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-10 22:08:31 -0300

    Update Program.cs

--- source/c#-exercises/exercise-9-revisado/exercise-8/Program.cs
+++ source/c#-exercises/exercise-9-revisado/exercise-8/Program.cs
@@ -7,7 +7,7 @@ namespace exercise9 {
         static void Main(string[] args) {
             Produto p = new Produto("TV", 500.00, 10);
 
-            p.Nome = "TV 4K";
+            p.Nome = "TV 4K"
             Console.WriteLine(p.Preco);
             Console.WriteLine(p.Nome);
         }
