0cf7947c45cab266930f7f739816d151d0118502
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-11 22:19:36 -0300

    upload

---
+++ source/c#-exercises/exercise-10/exercise-10/Program.cs
@@ -0,0 +1,42 @@
+﻿using exercise_10;
+using System;
+using System.Globalization;
+
+namespace exercise10 {
+    internal class Program {
+        static void Main(string[] args) {
+            ContaBancaria conta;
+            Console.Write("Entre com os dados da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            } else {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para retirada: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
