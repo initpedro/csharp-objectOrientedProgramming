eb76c621ec5ffd5010a90305040eb16be9105a03
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-12 22:17:24 -0300

    upload

---
+++ source/c#-exercises/exercise-test/exercise-test-struct/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_test_struct {
+    internal class Point {
+        public double X;
+        public double Y;
+
+        public override string ToString() {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ source/c#-exercises/exercise-test/exercise-test-struct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+
+namespace exercisetest {
+    class Program {
+        static void Main(string[] args) {
+            //double? x = null;
+            //double? y = 10.0;
+
+            //// O valor que tem, ou nulo
+            //Console.WriteLine(x.GetValueOrDefault());
+            //Console.WriteLine(y.GetValueOrDefault());
+
+            //// False = sem valor
+            //Console.WriteLine(x.HasValue);
+            //// True = com valor
+            //Console.WriteLine(y.HasValue);
+
+            //if (x.HasValue)
+            //    Console.WriteLine(x.Value);
+            //else
+            //    Console.WriteLine("X is null");
+
+            //if (y.HasValue)
+            //    Console.WriteLine(y.Value);
+            //else
+            //    Console.WriteLine("Y is null");
+
+            double? x = null;
+            double? y = 10.0;
+            double a = x ?? 5.0;
+            double b = y ?? 5.0;
+
+            // Jogou 5 pq o x é nulo
+            Console.WriteLine(a);
+            // Jogou 10 pq o y não é nulo, e ele já valia 10
+            Console.WriteLine(b);
+        }
+    }
+}
