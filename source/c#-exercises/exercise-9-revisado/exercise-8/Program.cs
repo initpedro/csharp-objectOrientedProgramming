0219a65cac51435462ab0b0f8f25840ee9ce3637
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-13 22:27:09 -0300

    upload exerciserevisado

--- source/c#-exercises/exercise-9-revisado/exercise-8/Program.cs
+++ source/c#-exercises/exercise-9-revisado/exercise-8/Program.cs
@@ -5,11 +5,11 @@ using System.Globalization;
 namespace exercise9 {
     internal class Program {
         static void Main(string[] args) {
-            Produto p = new Produto("TV", 500.00, 
+            Produto p = new Produto("TV", 500.00, 10);
 
             p.Nome = "TV 4K";
-            Console.WriteLine(p.Preco
-            Console.WriteLine(p.Nom
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Nome);
         }
     }
 }
