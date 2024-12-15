bb46da5d09bf1d125e5cd92b9bce1dc2e51ea270
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-15 20:27:06 -0300

    upload exercise19

---
+++ source/c#-exercises/exercise-19/exercise-19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using exercise_19;
+
+namespace exercise19
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int seachId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == seachId);
+            if (emp != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            } else {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
