2161f32c45ebf6b202152ff561703eb6024a5239
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-15 20:28:46 -0300

    Update Program.cs

--- source/c#-exercises/exercise-20/exercise-20/Program.cs
+++ source/c#-exercises/exercise-20/exercise-20/Program.cs
@@ -3,7 +3,7 @@
 namespace exercise20
 {
     internal class Program {
-        static void Main(string[]
+        static void Main(stri
             double[,] mat = new double[2, 4];
 
             // Elementos na mat
