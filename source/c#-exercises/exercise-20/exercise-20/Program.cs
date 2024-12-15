7d532a2f52e611e15b4804309faa71d98e4066ff
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-15 20:27:33 -0300

    upload exercise20

---
+++ source/c#-exercises/exercise-20/exercise-20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise20
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double[,] mat = new double[2, 4];
+
+            // Elementos na matriz
+            Console.WriteLine(mat.Length);
+
+            // Quanto é a primeira dimensão da matriz (linhas) e percorre as colunas
+            Console.WriteLine(mat.Rank);
+
+            // Quantidade de linhas
+            Console.WriteLine(mat.GetLength(0));
+
+            // Segunda dimensão, quantidade de colunas
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
