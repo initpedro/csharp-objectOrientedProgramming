b4aee3c01fc79e94ecc879fd9da57074c690791e
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-14 22:14:58 -0300

    upload exercise12

---
+++ source/c#-exercises/exercise-12/exercise-12/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace exercise_12 {
+    internal class Product {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ source/c#-exercises/exercise-12/exercise-12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+using exercise_12;
+
+namespace exercise12
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++) {
+                // Somar o Price
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"AVERAGE HEIGHT = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
