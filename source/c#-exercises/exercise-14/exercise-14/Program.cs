f01a46d389e52c34b2a80403b40348ef46d6cd0d
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-14 22:16:10 -0300

    upload exercise14

---
+++ source/c#-exercises/exercise-14/exercise-14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using exercise_14;
+
+namespace exercise14 {
+    internal class Program {
+        static void Main(string[] args) {
+            int s1 = Calculator.Sum(1, 2, 3, 4, 5);
+            int s2 = Calculator.Sum(2, 8);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
