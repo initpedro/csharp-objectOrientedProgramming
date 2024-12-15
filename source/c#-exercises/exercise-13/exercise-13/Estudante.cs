45d8af1d073e1afa3453bad9a5b845e87e82886b
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-14 22:15:39 -0300

    upload exercise13

---
+++ source/c#-exercises/exercise-13/exercise-13/Estudante.cs
@@ -0,0 +1,16 @@
+﻿namespace exercise_13 {
+    internal class Estudante {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return $"{Nome}, {Email}";
+        }
+    }
+}
