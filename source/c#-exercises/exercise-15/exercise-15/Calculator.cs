80aaec04b5ba1f8b750c5472a25064b8378184df
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-14 22:16:39 -0300

    upload exercise15

---
+++ source/c#-exercises/exercise-15/exercise-15/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace exercise_15 {
+    internal class Calculator {
+
+        // ref - faz o valor referência para variável original
+        public static void Triple(ref int x) {
+            x *= 3;
+        }
+    }
+}
---
+++ source/c#-exercises/exercise-15/exercise-15/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using exercise_15;
+
+namespace exercise15
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
