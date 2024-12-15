2e4ce16a723b6e2a08160117f4d718cf2af76d73
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-14 22:17:06 -0300

    upload exercise16

---
+++ source/c#-exercises/exercise-16/exercise-16/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+namespace exercise_16 {
+    internal class Calculator {
+        // out, armazenou o dado de out int result, vai ser a cópia da variável referência "a" no programa principal
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ source/c#-exercises/exercise-16/exercise-16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using exercise_16;
+
+namespace exercise16
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
