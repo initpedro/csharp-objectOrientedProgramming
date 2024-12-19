1347fb78ed322f99c6c89695b490d913f84b3fc4
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-18 21:47:08 -0300

    update exercise

--- source/c#-exercises/exercise-23/exercise-23/Program.cs
+++ source/c#-exercises/exercise-23/exercise-23/Program.cs
@@ -5,19 +5,20 @@ namespace exercise23 {
     internal class Program {
         static void Main(string[] args) {
 
-            // Devolve o instante atu
+            // Devolve o instante atual
             // DateTime d1 = DateTime.Now;
-            // Console.WriteLi
+            // Console.WriteLine(d1);
             // Console.WriteLine(d1.Ticks);
 
             //DateTime d1 = new DateTime(2018, 11, 25);
-            //Console.WriteLine(d1
+            //Console.WriteLine(d1);
 
             //DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 03);
             //Console.WriteLine(d2);
 
             //DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 03, 500);
-            //Console.WriteLine(d3
+            //Console.WriteLine(d3);
+
             //DateTime d4 = DateTime.Now;
             //Console.WriteLine(d4);
 
