eab938d41d4c4e61b8c57c4f5670402e91b88a44
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-19 22:13:21 -0300

    upload exercise26

---
+++ source/c#-exercises/exercise-26/exercise-26/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercise26 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            DateTime d = new DateTime(2000, 10, 15);
+            DateTime d2 = new DateTime(2000, 10, 18);
+
+            TimeSpan t = d2.Subtract(d);
+            Console.WriteLine(t);
+        }
+    }
+}
