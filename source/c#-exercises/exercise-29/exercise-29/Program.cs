9c62d58b918b6ea57bc1e1030505be0551ef61d5
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-21 22:28:48 -0300

    upload exercise29

---
+++ source/c#-exercises/exercise-29/exercise-29/Program.cs
@@ -0,0 +1,22 @@
+﻿using exercise_29.Entities;
+using exercise_29.Entities.Enums;
+using System;
+
+namespace exercise29 {
+    internal class Program {
+        static void Main(string[] args) {
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = exercise_29.Entities.Enums.OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
