3c7ba362c78ca7f1aa55c9d2cbc627076a500f43
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-22 21:48:21 -0300

    Update Program.cs

--- source/c#-exercises/exercise-29/exercise-29/Program.cs
+++ source/c#-exercises/exercise-29/exercise-29/Program.cs
@@ -8,12 +8,12 @@ namespace exercise29 {
             Order order = new Order {
                 Id = 1080,
                 Moment = DateTime.Now,
-                Status = exercise_29.Entities.Enums.OrderStatus.PendingPayme
+                Status = exercise_29.Entities.Enums.OrderStatus.PendingPayment
             };
             Console.WriteLine(order);
 
             string txt = OrderStatus.PendingPayment.ToString();
-            Console.WriteLine(txt);
+            Console.WriteLine(txt
 
             OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
             Console.WriteLine(os);
