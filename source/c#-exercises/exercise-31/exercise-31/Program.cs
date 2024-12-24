b27f2bc4de62493a9b307b816974f7903e0ba783
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-24 19:36:34 -0300

    Update Program.cs

--- source/c#-exercises/exercise-31/exercise-31/Program.cs
+++ source/c#-exercises/exercise-31/exercise-31/Program.cs
@@ -9,7 +9,7 @@ namespace exercise31 {
             Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"),
                 "Traveling to new Zeeland",
                 "\nI'm going to visit this wonderful contry!",
-                12);
+                12
             p1.AddComment(c1);
             p1.AddComment(c2);
 
