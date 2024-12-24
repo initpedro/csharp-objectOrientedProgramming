b604e79f3195dbbdfc04d6d9e16d777a949e740e
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-24 19:34:38 -0300

    commit one

---
+++ source/c#-exercises/exercise-31/exercise-31/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using exercise_31.Entities;
+
+namespace exercise31 {
+    internal class Program {
+        static void Main(string[] args) {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to new Zeeland",
+                "\nI'm going to visit this wonderful contry!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys!",
+                "\nSee you tomorrow",
+                12);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
