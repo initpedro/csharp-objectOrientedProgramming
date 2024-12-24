a0b88a503f964a8b419f8d74d95be78c89cee698
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-24 19:36:48 -0300

    Update Program.cs

--- source/c#-exercises/exercise-31/exercise-31/Program.cs
+++ source/c#-exercises/exercise-31/exercise-31/Program.cs
@@ -4,7 +4,7 @@ using exercise_31.Entities;
 namespace exercise31 {
     internal class Program {
         static void Main(string[] args) {
-            Comment c1 = new Comment("Have a nice trip");
+            Comment c1 = new Comment("Have a nice trip"
             Comment c2 = new Comment("Wow that's awesome!");
             Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"
                 "Traveling to new Zeeland",
