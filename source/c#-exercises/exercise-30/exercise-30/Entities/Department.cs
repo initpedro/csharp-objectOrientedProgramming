15e576ca88959df9d474fdce749a37e2cd9a8d91
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-24 19:33:53 -0300

    upload exercise30

---
+++ source/c#-exercises/exercise-30/exercise-30/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_30.Entities {
+    internal class Department {
+
+        public string Name { get; set; }
+
+        public Department() {
+
+        }
+
+        public Department(string name) {
+            Name = name;
+        }
+    }
+}
