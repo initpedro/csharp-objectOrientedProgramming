4fe115a1b1c46e9fb9386c6b520f96ad17ed47e0
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-24 19:36:43 -0300

    Update Program.cs

--- source/c#-exercises/exercise-31/exercise-31/Program.cs
+++ source/c#-exercises/exercise-31/exercise-31/Program.cs
@@ -6,7 +6,7 @@ namespace exercise31 {
         static void Main(string[] args) {
             Comment c1 = new Comment("Have a nice trip");
             Comment c2 = new Comment("Wow that's awesome!");
-            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"),
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"
                 "Traveling to new Zeeland",
                 "\nI'm going to visit this wonderful contry!
                 12
