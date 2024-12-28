b2590bdce5927b60d25fdcc6c82254ba09da3532
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-27 21:16:09 -0300

    upload exercise33

---
+++ source/c#-exercises/exercise-33/exercise-33/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace exercise_33.Entities {
+    internal class OutsourcedEmployee :Employee {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() {
+
+        }
+        public override double Payment() {
+            // Reaproveitar a lógica do pagamento comum
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour) {
+            AdditionalCharge = additionalCharge;
+        }
+    }
+}
