20500ba085e1adbef4daa671d259a0f122277b85
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-27 21:17:15 -0300

    Update Program.cs

--- source/c#-exercises/exercise-34/exercise-34/Program.cs
+++ source/c#-exercises/exercise-34/exercise-34/Program.cs
@@ -8,7 +8,7 @@ namespace exercise34 {
         static void Main(string[] args) {
             List<Product> list = new List<Product>();
 
-            Console.Write("Enter the number of products: ");
+            Console.Write("Enter the number of products: 
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= n; i++) {
