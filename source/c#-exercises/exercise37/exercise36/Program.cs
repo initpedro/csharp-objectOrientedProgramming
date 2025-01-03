5bb3379ea3ccbaf69a672ffb149ba29026c3ccbd
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-02 23:25:40 -0300

    upload exercise37

---
+++ source/c#-exercises/exercise37/exercise36/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise36 {
+    internal class Program {
+        static void Main(string[] args) {
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            } catch (DivideByZeroException ex) {
+                Console.WriteLine($"Error! {ex.Message}");
+            } catch (FormatException e) {
+                Console.WriteLine($"Format error! {e.Message}");
+            }
+        }
+    }
+}
