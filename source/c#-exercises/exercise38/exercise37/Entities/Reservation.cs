9b7bc2d66d46438e24dec81850cb73240d72463e
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-02 23:26:19 -0300

    upload exercise38

---
+++ source/c#-exercises/exercise38/exercise37/Entities/Reservation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise37.Entities {
+    internal class Reservation {
+        public int RoomNumer { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() {
+
+        }
+
+        public Reservation(int roomNumer, DateTime checkIn, DateTime checkOut) {
+            RoomNumer = roomNumer;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration() {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut) {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString() {
+            return $"Room {RoomNumer}, check-ing: {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights.";
+        }
+    }
+}
