7aafae9841f8444c31ddb08b85beb2b4a4f6345e
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-04 18:17:12 -0300

    upload exercise41

---
+++ source/c#-exercises/exercise-41/exercise-41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace exercise41 {
+    internal class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"C:\Windows\Temp\File1.txt";
+            string targetPath = @"C:\Windows\Temp\File2.txt";
+
+            try {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
