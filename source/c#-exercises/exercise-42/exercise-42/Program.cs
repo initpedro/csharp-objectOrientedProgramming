4bab296f154ac88f16add8b429373e39a8c7f410
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-04 18:17:47 -0300

    upload exercise42

---
+++ source/c#-exercises/exercise-42/exercise-42/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+
+namespace exercise42 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = @"c:\windows\temp\File1.txt";
+            // Stream genérica
+            FileStream fs = null;
+            // Stream particular otimizada
+            StreamReader sr = null;
+            try {
+                // Instanciado para abrir - Conversa com o Sistema Operacional
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            } catch (IOException ex) {
+                Console.WriteLine($"An error accurred");
+                Console.WriteLine(ex.Message);
+            }
+            
+            // Fechar streams
+            finally {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
