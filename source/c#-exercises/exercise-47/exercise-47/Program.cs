8845f5da42698032a9e65bd87c929c4c6e9d7129
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-06 23:39:38 -0300

    upload exercise47

---
+++ source/c#-exercises/exercise-47/exercise-47/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime;
+
+namespace exercise47 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = @"c:\windows\temp\file1.txt";
+            // Mostra o caractere de separação
+            Console.WriteLine($"DirectorySeparatorChar: {Path.DirectorySeparatorChar}");
+            // Porção do string, deixando de lado a pasta no caminho
+            Console.WriteLine($"GetDirectoryName: {Path.GetDirectoryName(path)}");
+            // Separar a pasta
+            Console.WriteLine($"PathSeparator: {Path.PathSeparator}");
+            // Nome do arquivo com extensão
+            Console.WriteLine($"GetFileName: {Path.GetFileName(path)}");
+            // Nome sem extensão
+            Console.WriteLine($"GetFileNameWithoutExtension: {Path.GetFileNameWithoutExtension(path)}");
+            // Extensão do arquivo
+            Console.WriteLine($"GetExtension: {Path.GetExtension(path)}");
+            // Todo caminho do arquivo
+            Console.WriteLine($"GetFullPath: {Path.GetFullPath(path)}");
+            // Caminho pasta temporária do sistema
+            Console.WriteLine($"GetTempPath: {Path.GetTempPath()}");
+        }
+    }
+}
