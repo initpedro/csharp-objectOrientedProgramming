d8f1a61dc32e0533f17c59fada2443978864a608
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-06 23:38:44 -0300

    upload exercise46

---
+++ source/c#-exercises/exercise-46/exercise-46/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace exercise46 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\Pedro\AppData\Local\Temp\Test";
+
+            try {
+                IEnumerable<string> folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine($"FOLDERS: ");
+                foreach (string s in folder) {
+                    Console.WriteLine(s);
+                }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine($"FILES: ");
+                foreach (string s in files) {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory($"{path}\\newfolder");
+                } catch (Exception ex) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
