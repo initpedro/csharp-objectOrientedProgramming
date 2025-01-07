cf4291226e5c81105a092d584146a5d833eb15c1
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2025-01-06 23:38:05 -0300

    upload exercise45

---
+++ source/c#-exercises/exercise-45/exercise-45/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise45 {
+    internal class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\windows\temp\File1.txt";
+            string targetPath = @"c:\windows\temp\File2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+
+                }
+
+            } catch (Exception ex) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
