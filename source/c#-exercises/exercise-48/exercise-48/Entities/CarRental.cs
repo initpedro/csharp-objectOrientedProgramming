239b9b1889d204762be7bc80ba07901352a06e83
Pedro <pedro16hf@gmail.com>
2025-01-08 22:22:17 -0300

    fix: CarRental

---
+++ source/c#-exercises/exercise-48/exercise-48/Entities/CarRental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_48.Entities {
+    internal class CarRental {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        // Invoice não entra, pois começa nulo
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle) {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
