0673d9e1cd9bbe083325ce8cea591924c0c7090d
Pedro <pedro16hf@gmail.com>
2025-01-08 22:39:57 -0300

    fix: services att

--- source/c#-exercises/exercise-48/exercise-48/Services/RentalService.cs
+++ source/c#-exercises/exercise-48/exercise-48/Services/RentalService.cs
@@ -10,13 +10,26 @@ namespace exercise_48.Services {
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalService(double pricePerHour, double pricePerDay) {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
         }
 
         public void ProcessInvoice(CarRental carRental) {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0;
+            if (duration.TotalHours <= 12.0) {
+                // Arredondar para cima com o Math.Ceiling
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            } else {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
 
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
