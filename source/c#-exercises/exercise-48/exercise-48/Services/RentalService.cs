f2003bd33eda1dacbfa1fc181df66b8326c7ada8
Pedro <pedro16hf@gmail.com>
2025-01-08 22:33:34 -0300

    build: new services RentalService

---
+++ source/c#-exercises/exercise-48/exercise-48/Services/RentalService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using exercise_48.Entities;
+
+namespace exercise_48.Services {
+    internal class RentalService {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay) {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental) {
+
+        }
+    }
+}
