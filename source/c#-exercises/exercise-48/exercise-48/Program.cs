d513379b7b5561f820760b34c9560ae62166f11c
Pedro <pedro16hf@gmail.com>
2025-01-08 22:27:38 -0300

    feat: Program

--- source/c#-exercises/exercise-48/exercise-48/Program.cs
+++ source/c#-exercises/exercise-48/exercise-48/Program.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using exercise_48.Entities;
+using System;
+using System.Globalization;
 
 namespace exercise48 {
     internal class Program {
         static void Main(string[] args) {
-            
+            Console.WriteLine($"Enter rental data: ");
+            Console.WriteLine($"Car Model: ");
+            string model = Console.ReadLine();
+
+            Console.WriteLine($"Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine($"Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
         }
     }
 }
