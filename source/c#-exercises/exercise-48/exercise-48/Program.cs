b5788ce649cb9345fca799d9577ba2295bcdf1c9
Pedro <pedro16hf@gmail.com>
2025-01-08 22:44:12 -0300

    feat: att program

--- source/c#-exercises/exercise-48/exercise-48/Program.cs
+++ source/c#-exercises/exercise-48/exercise-48/Program.cs
@@ -1,19 +1,31 @@
 ﻿using exercise_48.Entities;
 using System;
 using System.Globalization;
+using exercise_48.Services;
 
 namespace exercise48 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine($"Enter rental data: ");
-            Console.WriteLine($"Car Model: ");
+            Console.Write($"Enter rental data: ");
+            Console.Write($"Car Model: ");
             string model = Console.ReadLine();
 
-            Console.WriteLine($"Pickup (dd/MM/yyyy hh:mm): ");
+            Console.Write($"Pickup (dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-            Console.WriteLine($"Return (dd/MM/yyyy hh:mm): ");
+            Console.Write($"Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write($"Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write($"Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalServcice = new RentalService(hour, day);
+            rentalServcice.ProcessInvoice(carRental);
+            Console.Write($"INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
