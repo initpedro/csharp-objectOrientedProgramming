390a250b2ffc9b36f2aa7b1b01f8d0fd6cc35c16
Pedro <pedro16hf@gmail.com>
2025-01-09 21:59:11 -0300

    feat: add ITaxService in Class

--- source/c#-exercises/exercise-48/exercise-48/Services/BrazilTaxService.cs
+++ source/c#-exercises/exercise-48/exercise-48/Services/BrazilTaxService.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace exercise_48.Services {
-    internal class BrazilTaxService {
+    internal class BrazilTaxService : ITaxService {
         public double Tax(double amount) {
             if (amount <= 100) {
                 return amount * 0.2;
