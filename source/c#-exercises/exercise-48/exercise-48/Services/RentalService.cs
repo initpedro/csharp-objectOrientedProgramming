48a2d82a09aabb4c86e097eaadd37a004625f946
Pedro <pedro16hf@gmail.com>
2025-01-10 22:52:06 -0300

    fix: Services

--- source/c#-exercises/exercise-48/exercise-48/Services/RentalService.cs
+++ source/c#-exercises/exercise-48/exercise-48/Services/RentalService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using exercise_48.Entities;
+﻿using exercise_48.Entities;
 
 namespace exercise_48.Services {
     internal class RentalService {
