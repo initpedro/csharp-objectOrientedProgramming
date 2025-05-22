f3e53adb273485a74c524822c97c4c2495ce0c7d
Pedro Fonseca <132118855+initpedro@users.noreply.github.com>
2025-05-21 21:29:01 -0300

    fix: upload exercise 🎉

---
+++ source/c#-exercises/exercise-51/exercise-51/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace exercise51 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\test.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream) {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list) {
+                        Console.WriteLine(str);
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
