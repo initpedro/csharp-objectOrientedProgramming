3476f2dc2684dfad7d6b3ea1acccdec4200c7d3d
Pedro Fonseca <132118855+initpedro@users.noreply.github.com>
2025-05-28 23:29:18 -0300

    fix: upload exercise 🎉

---
+++ source/c#-exercises/exercise-54/exercise-54/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using exercise_54.Entities;
+
+namespace exercise54 {
+    internal class Program {
+        static void Main(string[] args) {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); // Referência de conteúdo
+            Console.WriteLine(a == b); // Referência de memória
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
