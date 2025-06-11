f97c8fae71d96d8be89055f2f7a9b54541da3e36
Pedro Fonseca <132118855+initpedro@users.noreply.github.com>
2025-06-10 23:25:34 -0300

    fix: upload exercise 🎉

---
+++ source/c#-exercises/exercise-60/exercise-60/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System {
+    static internal class StringExtensions {
+        public static string Cut(this string thisObj, int count) {
+            if (thisObj.Length <= count) {
+                return thisObj;
+            } else {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ source/c#-exercises/exercise-60/exercise-60/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace exercise60 {
+    internal class Program {
+        static void Main(string[] args) {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine($"{s1.Cut(10)}");
+        }
+    }
+}
